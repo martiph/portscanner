69d2ab10d93090b1f539a47f0539820084ce844f
martiph <philip_marti@besonet.ch>
2020-09-21 17:57:50 +0200

    updated portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,11 +1,13 @@
+import time
 import socket
 import argparse
 
 parser = argparse.ArgumentParser(description='A simple portscanner')
 parser.add_argument('host', help='Specify the host to scan')
 parser.add_argument('--portrange', '-p', type=str, help='Specify a port range to scan')
-parser.add_argument('--service', '-s', type=str, help='Specify a service name to scan the underlying port')
+parser.add_argument('--service', '-s', type=str, help='Specify a service name to scan the associated port')
 parser.add_argument('--timeout', '-t', type=int, help='Set the timeout for the tcp connections')
+parser.add_argument('--fingerprint', '-f', action='store_true', help='Try to perform service fingerprinting')
 parser.add_argument('--verbose', '-v', action='store_true', help='Show verbose output')
 arguments = parser.parse_args()
 
@@ -15,6 +17,45 @@ def printv(*args, **kwargs):
         print(*args, **kwargs)
 
 
+def parse_portstring(portrange: str):
+    ports = list()
+    if ',' in portrange and '-' in portrange:
+        portrange_list = portrange.split(',')
+        for port in portrange_list:
+            if '-' in port:
+                portsubrange = port.split('-')
+                for p in range(int(portsubrange[0]), int(portsubrange[1])):
+                    ports.append(p)
+            else:
+                ports.append(int(port))
+    elif ',' in portrange:
+        portrange_list = portrange.split(',')
+        for port in portrange_list:
+            ports.append(int(port))
+    elif "-" in portrange:
+        portrange_list = portrange.split('-')
+        for i in range(int(portrange_list[0]), int(portrange_list[1])):
+            ports.append(i)
+    else:
+        ports.append(int(portrange))
+
+    ports = list(dict.fromkeys(ports))  # remove duplicate entries
+    return ports
+
+
+def parse_service_to_port(service):
+    ports = list()
+    printv("Translating service to port number for service(s): {}".format(service))
+    if ',' in service:
+        services = service.split(',')
+        [ports.append(socket.getservbyname(service.strip())) for service in services]
+    else:
+        ports.append(socket.getservbyname(service))
+    printv("The following ports were extracted from the provided services: {}".format(
+        ', '.join([str(port) for port in ports])))
+    return ports
+
+
 def tcp_connect_port(host: str, port: int, timeout: int):
     ip_address = socket.gethostbyname(host)
     status = None
@@ -22,65 +63,56 @@ def tcp_connect_port(host: str, port: int, timeout: int):
     s.settimeout(timeout)
     printv("Timeout set to {}".format(timeout))
     try:
-        connection = s.connect((ip_address, port))
-        if connection is None:
-            printv('Connection to port {} successful'.format(port))
-            status = 'open'
+        s.connect((ip_address, port))
+        if arguments.fingerprint:
+            try:
+                data = s.recv(4096).decode('utf8')
+                printv("Performing service detection on port {}".format(port))
+                service_name = socket.getservbyport(port)
+                printv("Service {} is in version {}".format(service_name, data))
+            except socket.timeout:
+                printv("")
+        printv('Connection to port {} successful'.format(port))
+        status = 'open'
     except ConnectionRefusedError:
         printv('Connection to port {} refused'.format(port))
         status = 'closed'
     except socket.timeout:
         printv('Connection to port {} timed out'.format(port))
         status = 'firewalled'
+    except Exception as e:
+        printv(e)
     finally:
+        printv("closing connection")
         s.close()
         return tuple((port, status))
 
 
-def tcp_connect_scan(host: str = 'localhost', portrange: str = '22', timeout: int = 2):
-    printv("Host: {}, Portrange: {}, Timeout: {}".format(host, portrange, timeout))
+def tcp_connect_scan(hosts: str = 'localhost', portrange: str = '22', timeout: int = 2):
+    printv("Host: {}, Portrange: {}, Timeout: {}".format(hosts, portrange, timeout))
     ports = parse_portstring(portrange)
-    if ports[1]:
-        for i in range(ports[0][0], ports[0][1]):
-            yield tcp_connect_port(host, i, timeout)
-    else:
-        for port in ports[0]:
+    hosts = [host.strip() for host in hosts.split(',')]
+    for host in hosts:
+        for port in ports:
             yield tcp_connect_port(host, port, timeout)
 
 
-def parse_portstring(portrange: str):
-    is_portrange = False
-    if '-' in portrange:
-        is_portrange = True
-        ports = portrange.split('-')
-        for i in range(len(ports)):
-            ports[i] = int(ports[i].strip())
-    elif ',' in portrange:
-        ports = portrange.split(',')
-        for i in range(len(ports)):
-            ports[i] = int(ports[i].strip())
-    else:
-        ports = [int(portrange)]
-
-    ports = list(dict.fromkeys(ports))  # remove duplicate entries
-    return (ports, is_portrange)
-
-
-def parse_service_to_port(service):
-    if ',' in service:
-        services = service.split(',')
-        ports = [socket.getservbyname(service.strip()) for service in services]
-    else:
-        ports = socket.getservbyname(service)
-    return ports
-
-
 def main():
-    if arguments.host and arguments.portrange and arguments.timeout is not None:
-        for result in tcp_connect_scan(arguments.host, arguments.portrange, arguments.timeout):
+    printv("Arguments are: {}".format(str(arguments)))
+    if arguments.service is not None:
+        service_port_list = parse_service_to_port(arguments.service)
+        service_port_list = ','.join([str(item) for item in service_port_list])
+        if arguments.portrange is not None:
+            portrange = arguments.portrange + ',' + service_port_list
+        else:
+            portrange = service_port_list
+    else:
+        portrange = arguments.portrange
+    if arguments.host and portrange and arguments.timeout is not None:
+        for result in tcp_connect_scan(arguments.host, portrange, arguments.timeout):
             print(result)
-    elif arguments.host and arguments.portrange is not None:
-        for result in tcp_connect_scan(arguments.host, arguments.portrange):
+    elif arguments.host and portrange is not None:
+        for result in tcp_connect_scan(arguments.host, portrange):
             print(result)
     elif arguments.host is not None:
         for result in tcp_connect_scan(arguments.host):
